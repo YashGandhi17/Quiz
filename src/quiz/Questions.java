ea9b4bd81a3a17a9bd91b481a11035e4371d0582
YashGandhi17 <43007052+YashGandhi17@users.noreply.github.com>
2019-04-05 00:28:57 +0530

    Add files via upload

--- src/quiz/Questions.java
+++ src/quiz/Questions.java
@@ -5,10 +5,11 @@
  */
 package quiz;
 import java.sql.*;
+import java.util.*;
 /**
  *
  * @author Vraj
- */    
+ */
 public class Questions{
     String question;
     String option1;
@@ -24,19 +25,42 @@ public class Questions{
         this.option4=option4;
         this.answer=answer;
     }
-   /* public static void main(String[] args) {
-       System.out.println("Hello"); 
-        try{
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       List<Questions> QuestionList=new ArrayList<Questions>();
+       int ans;
+       int Score;
+      try{
             Connection c=DriverManager.getConnection("jdbc:ucanaccess://C:\\Users\\Vraj\\Documents\\NetBeansProjects\\Quiz\\src\\quiz\\Quiz.accdb");
             Statement st=c.createStatement();
-            ResultSet rs=st.executeQuery("select * from questions");
-            while(rs.next()){
-                System.out.println(rs.getString(1) + rs.getString(2)+"Hello");
-            }
-
+            ResultSet rs=st.executeQuery("select * from questions order by rand() limit 10");
         }
         catch(SQLException ee){System.out.println(ee);}
 
-    }*/
+        while(rs.next()){
+            QuestionList.add(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getInt(7));
+        }
+        for(Questions Q:QuestionList){
+          System.out.println(Q.question);
+          System.out.println(Q.option1);
+          System.out.println(Q.option2);
+          System.out.println(Q.option3);
+          System.out.println(Q.option4);
+
+          System.out.println("Enter your answer in 1/2/3/4");
+          ans=s.nextInt();
+
+          if(ans==Q.answer)
+          {
+            System.out.println("correct answer");
+            Score++;
+          }
+          else {
+            System.out.println("Ooops!Wrong answer");
+          }
+
+        }
+        System.out.println("Your Final Score Is:"+Score);
+
+    }
 }
-    
