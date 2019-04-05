2676a589a96f5163c6fbf962e81fa8d5ed49e9bc
YashGandhi17 <43007052+YashGandhi17@users.noreply.github.com>
2019-04-05 13:54:52 +0530

    Add files via upload

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quiz;
 
 import java.sql.Connection;
@@ -10,32 +5,61 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
- * @author Vraj
+ * @author Vraj,Yash
  */
 public class Quiz {
     
     public static void main(String[] args) {
         int limit=10;
-        int i=0;
-        Questions []question= new Questions[limit];
+        int index=0;
+        Scanner s=new Scanner(System.in);
+        int ans;
+        int score=0;
+        ArrayList<Questions> question=new ArrayList<Questions>();
         try{
-            Connection c=DriverManager.getConnection("jdbc:ucanaccess://C:\\Users\\Vraj\\Documents\\NetBeansProjects\\Quiz\\src\\quiz\\Quiz.accdb");
+            Connection c;
+            c = DriverManager.getConnection("jdbc:ucanaccess://D:\\yash\\Atom\\Quiz\\Quiz-master\\Quiz-master\\src\\quiz\\Quiz.accdb");
             Statement st=c.createStatement();
-            ResultSet rs=st.executeQuery("select * from questions order by rand() limit 1");
+            ResultSet rs=st.executeQuery("select * from questions order by rand() limit 10");
             while(rs.next()){
-                question[i]=new Questions(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getInt(7));
-                System.out.println(rs.getString(1) + rs.getString(2));
-                i++;
+                Questions Qs;
+                Qs=new Questions(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getInt(7));
+                question.add(Qs);
+                index++;
             }
-
+            c.close();
         }
         catch(SQLException ee){
             System.out.println(ee);
         }
-        System.out.println(question[0].question + question[0].option1 +question[0].option2+question[0].option3+question[0].option4+question[0].answer);
+        for(Questions Q:question){
+          System.out.println("Q"+". "+Q.question);
+          System.out.println("1. "+Q.option1);
+          System.out.println("2. "+Q.option2);
+          System.out.println("3. "+Q.option3);
+          System.out.println("4. "+Q.option4);
+
+          System.out.println("Enter your answer in 1/2/3/4");
+          ans=s.nextInt();
+
+          if(ans==Q.answer)
+          {
+            System.out.println("correct answer");
+            score++;
+          }
+          else {
+            System.out.println("Ooops!Wrong answer");
+          }
+
+        }
+        System.out.println("Your Final Score Is:"+score);
+        
     }
     
 }
